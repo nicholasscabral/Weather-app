a9817595a576df23e6280e6028f232979f528511
Ygor <yluiz684@gmail.com>
2020-08-12 15:14:42 -0300

    added portuguese translation

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-var tempIcon = document.querySelector('.weather-icon')
-var temperature = document.querySelector('.temperature-value')
-var description = document.querySelector('.temperature-description')
-var local = document.querySelector('.location')
-var inputValue = document.querySelector('#inputValue')
-var button = document.querySelector('#button')
+var tempIcon = document.querySelector(".weather-icon");
+var temperature = document.querySelector(".temperature-value");
+var description = document.querySelector(".temperature-description");
+var local = document.querySelector(".location");
+var inputValue = document.querySelector("#inputValue");
+var button = document.querySelector("#button");
 
-button.addEventListener('click', getCityData)
+button.addEventListener("click", getCityData);
 
 function convertTemperature(temperature) {
-    let celsius = Math.floor(temperature - 273)
-    return celsius + '°C'
+  let celsius = Math.floor(temperature - 273);
+  return celsius + "°C";
 }
 
 function getCityData() {
-    try {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=18154398977a537bd278e8d87bb29dc9')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            var city = data['name']
-            var temp = data['main']['temp']
-            var desc = data['weather'][0]['description']
-            var country = data['sys']['country']
-            var iconValue = data['weather'][0]['icon']
+  try {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        inputValue.value +
+        "&appid=18154398977a537bd278e8d87bb29dc9&lang=pt_br"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        var city = data["name"];
+        var temp = data["main"]["temp"];
+        var desc = data["weather"][0]["description"];
+        var country = data["sys"]["country"];
+        var iconValue = data["weather"][0]["icon"];
 
-            local.innerHTML = `<p>${city}, ${country}</p>`
-            temperature.innerHTML = `<p> ${convertTemperature(temp)}</p>`
-            description.innerHTML = `<p> ${desc} </p>`
-            tempIcon.innerHTML = `<img src="icons/${iconValue}.png">`
-
-        })
-    } catch (error) {
-        alert(error)
-    }    
+        local.innerHTML = `<p>${city}, ${country}</p>`;
+        temperature.innerHTML = `<p> ${convertTemperature(temp)}</p>`;
+        description.innerHTML = `<p> ${desc} </p>`;
+        tempIcon.innerHTML = `<img src="icons/${iconValue}.png">`;
+      });
+  } catch (error) {
+    alert(error);
+  }
 }
-
-
-
