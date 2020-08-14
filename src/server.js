aef4a918f3a7f7ada284dc2a36a753f859467d16
nicholasscabral <nicholasscabral@gmail.com>
2020-08-14 19:08:47 -0300

    init server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const server = express()
+
+server.use(express.static('public'))
+
+.get('/', (req, res) => {
+    return res.sendFile(__dirname + "/index.html")
+})
+
+.listen(7000, () => {
+    console.log('servidor iniciado/reiniciado')
+})
+
